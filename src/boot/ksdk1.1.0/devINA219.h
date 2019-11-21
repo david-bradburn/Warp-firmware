400dc52f2a55fe20187d118bdafedb4a81d9196c
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2019-11-21 20:59:27 +0000

    truncation fix attempt 1

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -6,12 +6,12 @@
 void		initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStatePointer);
 WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
 WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister,
-					uint8_t payloadBtye,
+					uint16_t payloadBtye,
 					uint16_t menuI2cPullupValue);
-WarpStatus	configureSensorINA219(uint8_t payloadF_SETUP, uint8_t payloadCTRL_REG1, uint16_t menuI2cPullupValue);
+// WarpStatus	configureSensorINA219(uint8_t payloadF_SETUP, uint8_t payloadCTRL_REG1, uint16_t menuI2cPullupValue);
 WarpStatus	readSensorSignalINA219(WarpTypeMask signal,
 					WarpSignalPrecision precision,
 					WarpSignalAccuracy accuracy,
 					WarpSignalReliability reliability,
 					WarpSignalNoise noise);
-void		printSensorDataINA219(bool hexModeFlag);
+// void		printSensorDataINA219(bool hexModeFlag);
