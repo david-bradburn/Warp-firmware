cf454ed9ebc7da46e84720caea5caac87444ad2e
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2019-12-18 22:46:51 +0000

    Update ergsystem.c

--- src/boot/ksdk1.1.0/ergsystem.c
+++ src/boot/ksdk1.1.0/ergsystem.c
@@ -4,33 +4,32 @@
 #include "ergsystem.h"
 
 int
-intsplittoarrayupper(int data) //so we can actually get the data in the format the OLED is designed for.
+intsplittoarraylower(int data) //so we can actually get the data in the format the OLED is designed for.
 {
-  int ar[2];
+  int ar1;
 
   if(data > 99)
   {
     data = 99;
   }
 
-  ar[0] = data % 10;
+  ar1 = data % 10;
 
 
-  return ar[0];
+  return ar1;
 }
 
 int
 intsplittoarraylower(int data) //so we can actually get the data in the format the OLED is designed for.
 {
-  int ar;
+  int ar2;
 
   if(data > 99)
   {
     data = 99;
   }
 
-  data -= (data % 10)*10;
-  ar = data % 1;
+  data -= (data % 10)/10;
 
-  return ar;
+  return data;
 }
