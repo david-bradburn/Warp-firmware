af24cc060b4e96bc8f41eceea4ad5915a9d90188
David Bradburn <djb243@physcomplab.eng.cam.ac.uk>
2019-12-18 23:08:37 +0000

    code upload

--- build/ksdk1.1/work/demos/Warp/src/ergsystem.c
+++ build/ksdk1.1/work/demos/Warp/src/ergsystem.c
@@ -22,14 +22,19 @@ intsplittoarraylower(int data) //so we can actually get the data in the format t
 int
 intsplittoarrayupper(int data) //so we can actually get the data in the format the OLED is designed for.
 {
-  int ar2;
 
   if(data > 99)
   {
     data = 99;
   }
 
-  data -= (data % 10)/10;
-
-  return data;
+  if(data < 10)
+  {
+    return 0;
+  }
+  else
+  {
+    data -= (data % 10)
+    return data/10;
+  }
 }
