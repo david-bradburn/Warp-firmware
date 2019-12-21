3698a60854ef56a146533d5bcba5fb855faf8fbf
David Bradburn <djb243@physcomplab.eng.cam.ac.uk>
2019-12-21 17:02:17 +0000

    code upload

--- build/ksdk1.1/work/demos/Warp/src/ergsystem.h
+++ build/ksdk1.1/work/demos/Warp/src/ergsystem.h
@@ -2,4 +2,4 @@
 
 int intsplittoarrayupper(int data);
 int intsplittoarraylower(int data);
-int * reduce_accel_array_resize_offset(int acc[], int offset, int length);
+//int * reduce_accel_array_resize_offset(int acc[], int offset, int length);
