ea144efaadd3dc8f5d799a9ccc70dd689a00e6bf
David Bradburn <djb243@physcomplab.eng.cam.ac.uk>
2019-12-21 19:37:43 +0000

    code upload

--- build/ksdk1.1/work/demos/Warp/src/ergsystem.c
+++ build/ksdk1.1/work/demos/Warp/src/ergsystem.c
@@ -41,71 +41,67 @@ intsplittoarrayupper(int data) //so we can actually get the data in the format t
 
 
 int *
-reduce_accel_array_resize_offset(int acc[], int offset, int length)
+reduce_accel_array_resize_offset(int acc[], int offset, int length, int index)
 {
-  static int accel_fit[96];
+  int accel_fit;
   int i;
-  int n;
-  //int n_1 = 1;
 
-  //size_t acc_len = (sizeof(acc)/sizeof(acc[0]));
-  int ar_len = (int)(sizeof(acc)/sizeof(acc[0]));
 
-  for(i = 0; i < ar_len; i++)
-  {
-    acc[i] -= offset;
-  }
 
-  if(ar_len > 96)
+  // for(i = 0; i < length; i++)
+  // {
+  //   acc[i] -= offset;
+  // }
+
+  if(length > 96)
   {
     float p;
     float q;
 
-    for(n = 0; n < 96; n++)
-    {
       p = 0;
       q = 0;
 
-      for(i = 0; i < ar_len; i++)
+      for(i = 0; i < length; i++)
       {
-        if(i/ar_len < n/96 && (i + 1)/ar_len > n/96)
+        if(i/length < index/96 && (i + 1)/length > index/96)
         {
-          p += acc[i]*(((i + 1)/ar_len) - n/96);
-          q += (((i + 1)/ar_len) - n/96)/(1/ar_len);
+          p += acc[i]*(((i + 1)/length) - index/96);
+          q += (((i + 1)/length) - index/96)/(1/length);
         }
-        else if(i/ar_len >= n/96 && i/ar_len < (n + 1)/96 && (i + 1)/ar_len < (n + 1)/96)
+        else if(i/length >= index/96 && (i + 1)/length < (index + 1)/96)
         {
           p += acc[i];
           q++;
         }
-        else if(i/ar_len < (n + 1)/96 && (i + 1)/ar_len > (n + 1)/96)
+        else if(i/length < (index + 1)/96 && (i + 1)/length > (index + 1)/96)
         {
-          p += acc[i]*((n + 1)/96 - ((i)/ar_len));
-          q += ((n + 1)/96 - ((i)/ar_len))/(1/ar_len);
+          p += acc[i]*((index + 1)/96 - ((i)/length));
+          q += ((index + 1)/96 - ((i)/length))/(1/length);
         }
 
-        if(i/ar_len > (n + 1)/96)
+        if(i/length > (index + 1)/96)
         {
-            break;
+          break;
         }
 
       }
 
-      accel_fit[n] = ((p/q) * 64)/(4096 * 2);
-    }
+
+      return ((p/q) * 64)/(4096 * 2);
+
 
   }
   else
   {
-    for(i = 0; i < ar_len; i++)
+    if(length > index)
     {
-      accel_fit[i] = acc[i];
+      return acc[length] *64/(4096 * 2);
     }
-    for(i = ar_len; i < 96; i++)
+    else
     {
-      accel_fit[i] = 0;
+      return 0;
     }
+
   }
 
-  return accel_fit;
 }
