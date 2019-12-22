72f0735caa9681356eaaed4308f981870bb24062
David Bradburn <djb243@physcomplab.eng.cam.ac.uk>
2019-12-22 12:33:14 +0000

    code upload

--- build/ksdk1.1/work/demos/Warp/src/warp-kl03-ksdk1.1-boot.c
+++ build/ksdk1.1/work/demos/Warp/src/warp-kl03-ksdk1.1-boot.c
@@ -2558,32 +2558,32 @@ devSSD1331init();
 				offset_av /= off_len;
 
 
-				SEGGER_RTT_printf(0, "\r\t\n %d\n", offset_av);
-
-				int length = 250;
-				int acc[length];
-
-				for(i = 0; i < length; i++)
-				{
-
-					readSensorRegisterMMA8451Q(0x01, 2);
-
-					hexoutx = ((deviceMMA8451QState.i2cBuffer[0] & 0xFF) << 6) | (deviceMMA8451QState.i2cBuffer[1] >> 2);
-
-					hexoutx = (hexoutx ^ (1 << 13)) - (1 << 13);
-
-					acc[i] += hexoutx - offset_av;
-
-					SEGGER_RTT_printf(0, "\r\t \n%d\n", acc[i]);
-
-				}
-
-
-				for (i = 0; i < 96; i++)
-				{
-					data[i] = reduce_accel_array_resize_offset(acc, length, i);
-				}
-				pullingforceprint(data, 96);
+				// SEGGER_RTT_printf(0, "\r\t\n %d\n", offset_av);
+				//
+				// int length = 250;
+				// int acc[length];
+				//
+				// for(i = 0; i < length; i++)
+				// {
+				//
+				// 	readSensorRegisterMMA8451Q(0x01, 2);
+				//
+				// 	hexoutx = ((deviceMMA8451QState.i2cBuffer[0] & 0xFF) << 6) | (deviceMMA8451QState.i2cBuffer[1] >> 2);
+				//
+				// 	hexoutx = (hexoutx ^ (1 << 13)) - (1 << 13);
+				//
+				// 	acc[i] += hexoutx - offset_av;
+				//
+				// 	SEGGER_RTT_printf(0, "\r\t \n%d\n", acc[i]);
+				//
+				// }
+				//
+				//
+				// for (i = 0; i < 96; i++)
+				// {
+				// 	data[i] = reduce_accel_array_resize_offset(acc, length, i);
+				// }
+				// pullingforceprint(data, 96);
 
 				break;
       }
