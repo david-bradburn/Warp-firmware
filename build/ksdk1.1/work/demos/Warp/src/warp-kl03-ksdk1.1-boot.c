e78434bc5e7313c060b9e899cf59ca573250d7a1
David Bradburn <djb243@physcomplab.eng.cam.ac.uk>
2019-12-22 16:15:00 +0000

    code upload

--- build/ksdk1.1/work/demos/Warp/src/warp-kl03-ksdk1.1-boot.c
+++ build/ksdk1.1/work/demos/Warp/src/warp-kl03-ksdk1.1-boot.c
@@ -2568,7 +2568,7 @@ devSSD1331init();
 				int16_t a;
 				for (i = 0; i < 3; i++)
 				{
-						length += (char2int(SEGGER_RTT_WaitKey()) * pow(10,(2 - i)));
+						length += ((int)char2int(SEGGER_RTT_WaitKey()) * (int)pow(10,(2 - i)));
 				}
 
 				SEGGER_RTT_printf(0, "\r\t\n%d\n", length);
