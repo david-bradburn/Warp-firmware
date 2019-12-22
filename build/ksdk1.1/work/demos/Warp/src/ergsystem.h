91b0601dd7d2aaca882b5664fd8e2145fad28345
David Bradburn <djb243@physcomplab.eng.cam.ac.uk>
2019-12-22 14:16:18 +0000

    code upload

--- build/ksdk1.1/work/demos/Warp/src/ergsystem.h
+++ build/ksdk1.1/work/demos/Warp/src/ergsystem.h
@@ -2,5 +2,5 @@
 
 int intsplittoarrayupper(int data);
 int intsplittoarraylower(int data);
-int reduce_accel_array_resize_offset(int acc[], int length, int index);
+int reduce_accel_array_resize_offset(int16_t acc[], uint8_t length, uint8_t index);
 //int offset_av_calc(int length, int menuI2cPullupValue);
