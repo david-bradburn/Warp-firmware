fb7b3093c01f96cbc6194eb716d06e790cc5f641
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2019-12-28 16:33:06 +0000

    test

--- tools/sdk/ksdk1.1.0/platform/drivers/src/spi/fsl_spi_master_driver.c
+++ tools/sdk/ksdk1.1.0/platform/drivers/src/spi/fsl_spi_master_driver.c
@@ -212,7 +212,7 @@ spi_status_t SPI_DRV_MasterTransferBlocking(uint32_t instance,
     errorStatus = SPI_DRV_MasterStartTransfer(instance, device);
     if (errorStatus != kStatus_SPI_Success)
     {
-        return errorStatus;
+        return 1;
     }
 
     /* As this is a synchronous transfer, wait until the transfer is complete.*/
@@ -939,4 +939,3 @@ void SPI_DRV_MasterIRQHandler(uint32_t instance)
 /*******************************************************************************
  * EOF
  ******************************************************************************/
-
