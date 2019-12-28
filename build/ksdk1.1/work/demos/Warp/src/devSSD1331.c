bed5968e1fdad6873e6eda53f382c3a366398e53
David Bradburn <djb243@physcomplab.eng.cam.ac.uk>
2019-12-28 17:13:39 +0000

    code upload

--- build/ksdk1.1/work/demos/Warp/src/devSSD1331.c
+++ build/ksdk1.1/work/demos/Warp/src/devSSD1331.c
@@ -120,8 +120,8 @@ default_colour(void)
 int
 recttest(void)
 {
-	uint8_t bytes[] = {kSSD1331CommandDRAWRECT, 0x00, 0x00, 0x5F, 0x3F, default_colour_arr, default_colour_arr};
-	spi_status_t status = writeCommandarr(bytes, 13);
+	uint8_t bytes[] = {kSSD1331CommandDRAWRECT, 0x00, 0x00, 0x5F, 0x3F, default_colour_arr, default_colour_arr, 0x00};
+	spi_status_t status = writeCommandarr(bytes, 11);
 
 	SEGGER_RTT_printf(0, "\r\t\n %d \n", status);
 
