82469f5be3d94f8f25e9a5da61a8aa34fb4d59eb
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2019-12-28 14:23:44 +0000

    Update ergsystem.c

--- src/boot/ksdk1.1.0/ergsystem.c
+++ src/boot/ksdk1.1.0/ergsystem.c
@@ -50,7 +50,7 @@ reduce_accel_array_resize_offset(int16_t acc[], uint16_t length, uint16_t index)
   if(length > 96)
   {
 
-    return (int16_t)((acc[(int16_t)(index*length/96)] * 64)/(4096 * 2));
+    return (int16_t)((acc[(int16_t)(index*length/96)] * 32)/(4096 * 2) + 32);
 
 
   }
@@ -58,7 +58,7 @@ reduce_accel_array_resize_offset(int16_t acc[], uint16_t length, uint16_t index)
   {
     if(length > index)
     {
-      return (int16_t)(acc[index] * 64/(4096 * 2));
+      return (int16_t)(acc[index] * 32/(4096 * 2) + 32);
     }
     else
     {
