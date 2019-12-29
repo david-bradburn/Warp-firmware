9db11501cb2802b624f73dc8aee10aa23833faf4
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2019-12-29 18:35:36 +0000

    Update warp-kl03-ksdk1.1-boot.c

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -2046,7 +2046,7 @@ devSSD1331init();
 			{
 				SEGGER_RTT_WriteString(0, "\r\n\tPrinting MMA8451Q register 1000 times \n");
 				enableI2Cpins(menuI2cPullupValue);
-				int16_t hexout;
+				int16_t hexoutx;
 
 				writeSensorRegisterMMA8451Q(0x09, 0x80, menuI2cPullupValue);
 				writeSensorRegisterMMA8451Q(0x2A, 0x01, menuI2cPullupValue);
