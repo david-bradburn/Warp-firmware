e31fb2db83a22960791073e70d5753467c87f067
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2020-01-03 16:26:09 +0000

    Update warp-kl03-ksdk1.1-boot.c

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1434,6 +1434,12 @@ devSSD1331init();
 								if(counter_99 > 5)
 								{
 									strokespermin = 99;
+
+									ar[0] = intsplittoarrayupper(strokespermin);
+									ar[1] = intsplittoarraylower(strokespermin);
+
+									writetoscreen(ar);
+
 									counter_99 = 0;
 								}
 								else
@@ -1443,11 +1449,16 @@ devSSD1331init();
 								}
 								SEGGER_RTT_printf(0, "\r\t %d, %d, %d, %d\n", hexoutx, hexoutx_prev, hexoutx_prev_prev, hexoutx_prev_prev_prev);
 							}
+							else
+							{
+								ar[0] = intsplittoarrayupper(strokespermin);
+								ar[1] = intsplittoarraylower(strokespermin);
 
-							ar[0] = intsplittoarrayupper(strokespermin);
-							ar[1] = intsplittoarraylower(strokespermin);
+								writetoscreen(ar);
 
-							writetoscreen(ar);
+								counter_99 = 0;
+							}
+
 
 							//SEGGER_RTT_printf(0, "\r\t%d\n", count);
 
